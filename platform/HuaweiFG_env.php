5898533a10abed3316cf1d44e7a88e990b0cffd7
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-04-22 19:20:31 +0800

    Update HuaweiFG_env.php

--- platform/HuaweiFG_env.php
+++ platform/HuaweiFG_env.php
@@ -68,8 +68,7 @@ function GetPathSetting($event, $context)
         $_SERVER['PHP_AUTH_PW'] = $basicAuth[1];
     }
     $_SERVER['REQUEST_SCHEME'] = $event['headers']['x-forwarded-proto'];
-    $_SERVER['host'] = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
-    //if ($_SERVER['HTTP_REFERER']!='') 
+    //$_SERVER['host'] = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
     $_SERVER['referhost'] = explode('/', $event['headers']['referer'])[2];
     $_SERVER['HTTP_TRANSLATE'] = $event['headers']['translate'];//'f'
     $_SERVER['_APP_SHARE_DIR'] = '/var/share/CFF/processrouter';
